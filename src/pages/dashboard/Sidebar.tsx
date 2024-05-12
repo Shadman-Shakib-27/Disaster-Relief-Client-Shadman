c394a94ed5c453938aec126b915929cd16c8a701
Shadman-Shakib-27 <s.s.a.27.8.1999@gmail.com>
2024-05-12 14:49:11 +0600

    Final Push.

--- src/pages/dashboard/Sidebar.tsx
+++ src/pages/dashboard/Sidebar.tsx
@@ -1,11 +1,22 @@
 import { NavLink } from "react-router-dom";
 import { cn } from "@/lib/utils";
-import { LayoutDashboardIcon, ServerIcon } from "lucide-react";
+import { Home, LayoutDashboardIcon, ServerIcon } from "lucide-react";
 
 const Sidebar = () => {
   return (
     <aside className="bg-light-gray h-screen shadow-md p-5 sticky top-0 left-0">
       <nav className="flex flex-col gap-2">
+        <NavLink
+          to="/"
+          className={() =>
+            cn(
+              "flex gap-2 bg-gray-400 p-3 rounded-sm hover:bg-secondary hover:text-white transition-all"
+            )
+          }
+        >
+          <Home className="w-5" />
+          <span className="text-lg">Home</span>
+        </NavLink>
         <NavLink
           to="/dashboard"
           className={() =>
